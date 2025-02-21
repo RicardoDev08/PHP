74a5b1142aa78a958335436abbad8593cac20e78
Ricardo <sousasilvaricardo9@gmail.com>
2025-02-21 08:05:01 -0400

    atividade 4

--- lista_exercicios/ex.004.php
+++ lista_exercicios/ex.004.php
@@ -3,25 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>atividade 4</title>
 </head>
 <body>
     <form method="POST">
         <p>Vamos calcular a área de um retângulo!</p>
         <br><p>Medida do comprimento:</p>
-        <br><input type="number" name="comprimento" id="comprimento">
+        <br><input type="number" name="comprimento" id="comprimento" required>
         <br><p>Medida da largura:</p>
-        <br><input type="number" name="Largura" id="largura">
+        <br><input type="number" name="largura" id="largura" required>
     </form>
     <?php
     $comprimento = $_POST["comprimento"];
-    $largura = $_POST["Largura"];
+    $largura = $_POST["largura"];
+
     function AreaRetangulo($comprimento, $largura){
-        $a = $comprimento * $largura;
-        return $AreaRetangulo;
+        $area = $comprimento * $largura;
+        return $area;
     };
-    echo "Área do retângulo:" $a;
-    
+    $resultado = AreaRetangulo($comprimento,$largura);
+    echo "A area do retângulo" .$resultado    
   
     
     
---
+++ lista_exercicios/ex.005.php
@@ -0,0 +1,16 @@
+5. Arrays
+Crie um programa que armazene em um array os 10 primeiros números da sequência de
+Fibonacci e, em seguida, exiba esses números na tela.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atividade 5</title>
+</head>
+<body>
+    <?php
+    
+    ?>
+</body>
+</html>
